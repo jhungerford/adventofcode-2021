8a3c8f5f17ad8de797fd258a76b0757f38e7d358
John Hungerford <jhungerford@gmail.com>
2021-12-07 06:47:58 -0700

    Day 7 part 1.

--- src/day7.rs
+++ src/day7.rs
@@ -17,25 +17,17 @@ fn load(filename: &str) -> Vec<i32> {
 
 /// Returns the least amount of fuel required to align all of the crabs.
 fn align_fuel(crabs: &Vec<i32>) -> i32 {
-    // The least amount of fuel aligns the crabs to the mode.
-    let crab_count: HashMap<i32, usize> = crabs.iter().fold(HashMap::new(), |mut counts, crab| {
-        *counts.entry(*crab).or_default() += 1;
-        counts
+    // Brute force - calculate the fuel at all of the positions between the crabs.
+    let (min, max) = crabs.iter().fold((0, 0), |(min, max), &crab| {
+        (min.min(crab), max.max(crab))
     });
 
-    let mode = crab_count.iter()
-        .max_by_key(|(_, count)| *count)
-        .map(|(crab, _)| *crab)
-        .unwrap();
+    (min ..= max)
+        .map(|target| crabs.iter().map(|&crab| (crab - target).abs()).sum())
+        .min()
+        .unwrap()
 
-    // Calculate the fuel required to shift each crab.
-    crabs.iter().map(|&crab| (crab - mode).abs()).sum()
 }
-
-// 16,1,2,0,4,2,7,1,2,14 - mode is 2, median is 49/10 = 4.9
-//0,10 - any number in range is 10
-//0,1,10 - any number is 11
-//0,1,1,10 - 1 is 11, any other number is 12
 
 #[test]
 fn test_align_fuel() {
