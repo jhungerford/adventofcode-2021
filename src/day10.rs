55210a11163f011d6aa804652c87dc6609eed756
John Hungerford <jhungerford@gmail.com>
2021-12-10 07:08:17 -0700

    Day 10 part 2.

--- src/day10.rs
+++ src/day10.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use itertools::Itertools;
 
 #[allow(dead_code)]
 pub fn solution() {
     let lines = load("input/day10.txt");
 
-    println!("Part 1: {}", total_score(&lines));
+    println!("Part 1: {}", syntax_score(&lines));
+    println!("Part 2: {}", autocomplete_score(&lines));
 }
 
 fn load(filename: &str) -> Vec<Line> {
@@ -18,8 +20,19 @@ fn load(filename: &str) -> Vec<Line> {
         .collect()
 }
 
-fn total_score(lines: &Vec<Line>) -> i32 {
-    lines.iter().map(|line| line.score()).sum()
+/// Returns the sum of syntax scores for lines that have mismatched closing characters.
+fn syntax_score(lines: &Vec<Line>) -> i32 {
+    lines.iter().flat_map(|line| line.syntax_score()).sum()
+}
+
+/// Returns the middle autocomplete score for incomplete lines.
+fn autocomplete_score(lines: &Vec<Line>) -> i64 {
+    let scores = lines.iter()
+        .flat_map(|line| line.autocomplete_score())
+        .sorted()
+        .collect::<Vec<i64>>();
+
+    scores[scores.len() / 2]
 }
 
 struct Line {
@@ -34,7 +47,7 @@ impl Line {
     /// Returns the syntax error score if this line is corrupted, or 0 otherwise.   A line has a
     /// syntax error if a chunk opens and closes with mismatched characters.  Closing with
     /// `)` = 3 points, `]` = 57, `}` = 1197, and `>` = 25137.
-    fn score(&self) -> i32 {
+    fn syntax_score(&self) -> Option<i32> {
         let scores = vec![
             (')', 3),
             (']', 57),
@@ -56,18 +69,56 @@ impl Line {
                 stack.push(m)
             } else if let Some(m) = stack.pop() {
                 if *m != c {
-                    return scores[&c];
+                    return Some(scores[&c]);
                 }
             }
         }
 
         // Line was incomplete, or didn't have any mismatches.
-        0
+        None
+    }
+
+    fn autocomplete_score(&self) -> Option<i64> {
+        let scores = vec![
+            (')', 1),
+            (']', 2),
+            ('}', 3),
+            ('>', 4),
+        ].into_iter().collect::<HashMap<char, i64>>();
+
+        let open_to_close = vec![
+            ('(', ')'),
+            ('[', ']'),
+            ('{', '}'),
+            ('<', '>'),
+        ].into_iter().collect::<HashMap<char, char>>();
+
+        let mut stack = Vec::new();
+
+        for c in self.line.chars() {
+            if let Some(m) = open_to_close.get(&c) {
+                stack.push(m)
+            } else if let Some(m) = stack.pop() {
+                if *m != c {
+                    // Line is corrupt - mismatched open and close.
+                    return None;
+                }
+            }
+        }
+
+        // Line is incomplete - calculate the score.
+        let mut score = 0;
+        while let Some(c) = stack.pop() {
+            score = score * 5 + scores[c];
+        }
+
+        Some(score)
     }
 }
 
 #[test]
 fn score_sample_file() {
     let lines = load("input/day10_sample.txt");
-    assert_eq!(26397, total_score(&lines));
+    assert_eq!(26397, syntax_score(&lines));
+    assert_eq!(288957, autocomplete_score(&lines));
 }
